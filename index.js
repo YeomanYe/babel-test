2b7dffd4f03ab56ff16ac1cf7cbaf3f0eb7d2698
yeming <tonyyeming@didiglobal.com>
2021-10-19 13:45:40 +0800

    feat: add pipe

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ setTimeout(() => {
     console.log('test babel', this);
 }, 100);
 
+const hello = (name) => 'hello ' + name;
+
+const n = 'world';
+n   |> hello(%)
+    |> console.log(%);
+
 class Test {
     
 }
